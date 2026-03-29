7635e0d0f80b33fa74c8573fea88db7176eb23af
Adityaa Mehra <adirakash@gmail.com>
2026-03-29 14:22:37 +0530

    Ran a normal test

--- test/test.py
+++ test/test.py
@@ -38,3 +38,10 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    await ClockCycles(dut.clk, 10)
+    dut._log.info("One more test case")
+    dut.ui_in.value = 5
+    dut.uio_in.value = 15
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 20
